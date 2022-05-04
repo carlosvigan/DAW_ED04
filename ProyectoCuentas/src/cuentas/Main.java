752d889ff736589731f3c91eb85113f1e3905e54
carlosvigan <cfernandezgarcia@hotmail.com>
2022-05-04 20:07:14 +0100

    Cambiamos cantidad a 695

--- ProyectoCuentas/src/cuentas/Main.java
+++ ProyectoCuentas/src/cuentas/Main.java
@@ -1,16 +1,16 @@
 package cuentas;
 /**
 
- * Esta clase contiene el método main y lanza las diferentes operativas que se desean realizar sobre la cuenta.
+ * Esta clase contiene el mï¿½todo main y lanza las diferentes operativas que se desean realizar sobre la cuenta.
 
- * @author: Carlos Fernández García
+ * @author: Carlos Fernï¿½ndez Garcï¿½a
 
  * @version: 1.0
  */
 public class Main {
 	/**
-     * Metodo main: Primer método que se ejecuta al lanzar la aplicación. Desde aqui vamos a llamar al método encargado de realizar las difentes operativas
-     * @param args Matriz de tipo String que recoge los valores que introduzcas a la hora de ejecutar tu aplicación.
+     * Metodo main: Primer mï¿½todo que se ejecuta al lanzar la aplicaciï¿½n. Desde aqui vamos a llamar al mï¿½todo encargado de realizar las difentes operativas
+     * @param args Matriz de tipo String que recoge los valores que introduzcas a la hora de ejecutar tu aplicaciï¿½n.
      */
     public static void main(String[] args) {
         CCuenta cuenta1;
@@ -18,13 +18,13 @@ public class Main {
 
         cuenta1 = new CCuenta("Juan Lopez","1000-2365-85-1230456789", 2500, 0);
         operativa_cuenta(cuenta1, 1, 2300);
-        operativa_cuenta(cuenta1, 2, 695);
+        operativa_cuenta(cuenta1, 2, 84);
 
     }
 	/**
      * Metodo operativa_cuenta: Metodo que nos permite realizar las operativas de retirar o ingresar dinero sobre la cuenta.
      * @param cuenta variable encargada de recibir el objeto instanciado de tipo CCuenta.
-     * @param operacion variable que nos permite indicar el tipo de operación que vamos a realizar. 1 es retirar y 2 ingresar.
+     * @param operacion variable que nos permite indicar el tipo de operaciï¿½n que vamos a realizar. 1 es retirar y 2 ingresar.
      * @param cantidad variable que recibe la cantidad de dinero a ingresar o retirar.
      */
 	private static void operativa_cuenta(CCuenta cuenta, int operacion, float cantidad) {
